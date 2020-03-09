79e6c2ac7eda922011bfaacfb20750f2420d15a0
Alice Richard <alicerichellerichard@gmail.com>
2020-03-09 20:22:33 -0300

    Inicio do projeto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="header">
+    <header class="header">
+        <div>
+          <img src="" alt="">
+          </div>
+          <div></div>
+            <nav class="nav-header">
+              <div class="router-link">
+                <router-link to="/">Pagina Inicial</router-link>
+                <router-link to="/">Sobre nós</router-link>
+                <router-link to="/">Contato</router-link>
+              </div>
+            </nav>
+          </div>
+       </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+}
+</script>
+<style scoped>
+header {
+  width: 100vw;
+  height: 80px;
+}
+</style>
